e1bdaf6342e56dde07f678f85115474c45e73ada
LiJiaqi <450103187@qq.com>
2025-01-10 15:05:41 +0800

    fix : delete none use code

--- lib/flutter_drag_selector.dart
+++ lib/flutter_drag_selector.dart
@@ -1,8 +1,3 @@
 
-import 'flutter_drag_selector_platform_interface.dart';
 
-class FlutterDragSelector {
-  Future<String?> getPlatformVersion() {
-    return FlutterDragSelectorPlatform.instance.getPlatformVersion();
-  }
-}
+library drag_selector;
--- test/flutter_drag_selector_method_channel_test.dart
+++ test/flutter_drag_selector_method_channel_test.dart
@@ -1,27 +1,8 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:flutter_drag_selector/flutter_drag_selector_method_channel.dart';
 
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
 
-  MethodChannelFlutterDragSelector platform = MethodChannelFlutterDragSelector();
-  const MethodChannel channel = MethodChannel('flutter_drag_selector');
 
-  setUp(() {
-    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger.setMockMethodCallHandler(
-      channel,
-      (MethodCall methodCall) async {
-        return '42';
-      },
-    );
-  });
-
-  tearDown(() {
-    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger.setMockMethodCallHandler(channel, null);
-  });
-
-  test('getPlatformVersion', () async {
-    expect(await platform.getPlatformVersion(), '42');
-  });
 }

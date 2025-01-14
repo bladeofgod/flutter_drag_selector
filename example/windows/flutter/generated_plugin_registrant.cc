c8f8d74831848faba5f26bb5ffb21985b0f41b42
LiJiaqi <450103187@qq.com>
2025-01-14 12:10:49 +0800

    fix : adjust pubspec.yaml

--- example/windows/flutter/generated_plugin_registrant.cc
+++ example/windows/flutter/generated_plugin_registrant.cc
@@ -6,9 +6,6 @@
 
 #include "generated_plugin_registrant.h"
 
-#include <flutter_drag_selector/flutter_drag_selector_plugin_c_api.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
-  FlutterDragSelectorPluginCApiRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("FlutterDragSelectorPluginCApi"));
 }

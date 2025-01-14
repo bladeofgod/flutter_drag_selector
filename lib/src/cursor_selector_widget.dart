8f96c85a2a5b876a15426e466381ee13971770d6
LiJiaqi <450103187@qq.com>
2025-01-14 11:56:41 +0800

    feat : add some comment

--- lib/src/cursor_selector_widget.dart
+++ lib/src/cursor_selector_widget.dart
@@ -2,39 +2,41 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 
-///Selector-rect changed callback
+/// Selector-rect changed callback
 typedef SelectZoneChangedCallback = void Function(DragUpdateDetails details, Rect selectZone);
 
-///Selectable container.
-///see also [SelectableItem]
+/// Selectable container.
+/// see also [SelectableItem]
 class CursorSelectorWidget extends StatefulWidget {
-  ///This child will be in selected zone.
+  /// This child will be in selected zone.
   /// e.g. [ListView], [GridView]
   /// see also [SelectableItem]
   final Widget child;
 
-  ///It will trigger this callback when cursor start drag.
+  /// It will trigger this callback when cursor start drag.
   /// * the event of this callback has been distinct.
   /// see also [CursorSelectorProvider.selectedStream], and you can directly listen it.
   final ValueChanged<(Key?, bool)>? selectedChangedCallback;
 
-  ///Cursor drag start will trigger this callback.
+  /// Cursor drag start will trigger this callback.
   final GestureDragStartCallback? dragStartCallback;
 
-  ///Cursor drag end will trigger this callback.
+  /// Cursor drag end will trigger this callback.
   final GestureDragEndCallback? dragEndCallback;
 
-  ///This callback is triggered when dragging the mouse cursor causes
-  ///the rect to changed.
+  /// This callback is triggered when dragging the mouse cursor causes the rect to changed.
+  ///
   /// * you can set this callback for listen drag details.
   /// * e.g. scroll [ListView]
   final SelectZoneChangedCallback? dragUpdateCallback;
 
-  ///The child scrollView's controller
+  /// The child scrollView's controller
   /// * user create select zone by dragging cursor, than scrolling mouse wheel,
   /// * than selector can continue select by this [scrollController].
   final ScrollController scrollController;
 
+  /// Manipulate action about scroll when cursor is dragging.
+  /// * if [selectorScrollHandler] is null, the value will be set as [DefaultCursorSelectorScrollHandler]
   final CursorSelectorScrollHandler? selectorScrollHandler;
 
   const CursorSelectorWidget({
@@ -59,15 +61,15 @@ class _CursorSelectorWidget extends State<CursorSelectorWidget> {
 
   Offset? _start;
 
-  ///Update ui of rect selection area.
+  /// Update ui of rect selection area.
   final _panUpdater = ValueNotifier<Offset?>(null);
 
-  ///selected event records
+  /// Selected event records
   /// * <Child-Key, isSelected>
   /// * start drag event will clear this records.
   final _eventRecords = <Key?, bool>{};
 
-  ///Listen the item-selected event and distinct it.
+  /// Listen the item-selected event and distinct it.
   void _distinctCallback((Key?, bool) event) {
     final key = event.$1;
     final isSelected = event.$2;
@@ -145,12 +147,12 @@ class _CursorSelectorWidget extends State<CursorSelectorWidget> {
   }
 }
 
-///The child who wanna be selected.
+/// The child who wanna be selected.
 /// * [key] must be set.
 class SelectableItem extends StatefulWidget {
   const SelectableItem({required super.key, required this.child});
 
-  ///the child of select-zone.
+  /// The child of select-zone.
   ///* e.g. children of [ListView] or [GridView], and so on.
   final Widget child;
 
@@ -179,9 +181,9 @@ class _SelectableItemState extends State<SelectableItem> with SelectTestBinding 
   }
 }
 
-///Helps item-widget test if it is in the selection area.
+/// Helps item-widget test if it is in the selection area.
 mixin SelectTestBinding<T extends StatefulWidget> on State<T> {
-  ///Test the cursor drag zone is overlay this child-widget
+  /// Test the cursor drag zone is overlay this child-widget
   bool selectTest(Rect selectRect, RenderObject? ancestor) {
     final rb = context.findRenderObject();
     if (rb is RenderBox) {
@@ -201,16 +203,16 @@ class CursorSelectorProvider extends InheritedWidget {
     return context.dependOnInheritedWidgetOfExactType<CursorSelectorProvider>();
   }
 
-  ///Cache the children who need test.
+  /// Cache the children who need test.
   final _selectorTest = <Key?, SelectTestBinding>{};
 
   final _selectorController = StreamController<(Key?, bool)>.broadcast();
 
-  ///Listen the selectable-item's test result.
-  ///(item's-key, isSelected)
+  /// Listen the selectable-item's test result.
+  /// (item's-key, isSelected)
   Stream<(Key?, bool)> get selectedStream => _selectorController.stream;
 
-  ///cursor drag-zone changed
+  /// Cursor drag-zone changed
   /// * [rect] cursor's drag zone
   /// * [ancestor] contained all children and drag-zone rect
   void cursorDragZoneChanged(Rect rect, RenderObject? ancestor) {
@@ -221,13 +223,13 @@ class CursorSelectorProvider extends InheritedWidget {
     }
   }
 
-  ///register child that who want be test in cursor-drag-zone
+  /// Register child that who want be test in cursor-drag-zone
   ///[key] is relate of your item
   void registerTester(Key? key, SelectTestBinding target) {
     _selectorTest[key] = target;
   }
 
-  ///remove the item that no need selected.
+  /// Remove the item that no need selected.
   void removeTester(Key? key) {
     _selectorTest.remove(key);
   }
@@ -238,8 +240,10 @@ class CursorSelectorProvider extends InheritedWidget {
   }
 }
 
+/// Responsible for handling the interaction with the child scrollview when the cursor is dragged.
 abstract class CursorSelectorScrollHandler {
 
+  /// Child scroll-view's scroll controller.
   final ScrollController childViewScrollController;
 
   CursorSelectorScrollHandler({required this.childViewScrollController});
@@ -249,7 +253,10 @@ abstract class CursorSelectorScrollHandler {
   void onPanEnd(DragEndDetails details);
 }
 
-
+/// Handling the scroll action while cursor dragging out of window(or select zone).
+///
+/// see also
+/// [_AutoScrollBinding]
 class DefaultCursorSelectorScrollHandler extends CursorSelectorScrollHandler with _AutoScrollBinding {
   DefaultCursorSelectorScrollHandler({required super.childViewScrollController});
 
@@ -277,7 +284,7 @@ class DefaultCursorSelectorScrollHandler extends CursorSelectorScrollHandler wit
   ScrollController get scrollController => childViewScrollController;
 }
 
-
+/// Control auto scroll
 mixin _AutoScrollBinding {
 
   bool _autoScrollSignal = false;
@@ -298,6 +305,8 @@ mixin _AutoScrollBinding {
     _autoScrollSignal = false;
   }
 
+  /// Start scrolling child-view that related [scrollController].
+  /// It will stop then call [stopAutoScroll]
   void startAutoScroll(double jumpPos) {
     if(_autoScrollSignal) return;
     _autoScrollSignal = true;
